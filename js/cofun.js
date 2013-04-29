ee72ffea4a4e077c20dfb5eeba8e0e76dd6e2220
omar morales <omarmorales865@gmail.com>
2013-04-29 19:57:18 -0400

    funciona

--- js/cofun.js
+++ js/cofun.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
 
     }
     var a = 6.25
-    var ap1 = new platillos (9.25, " Egg Roll ", "savory bits of beef and pork sauteed with chinese vegetables in a crispy golden wrapper. ");
+    var ck1 = new platillos (9.25, " Egg Roll ", "savory bits of beef and pork sauteed with chinese vegetables in a crispy golden wrapper. ");
     var ap2 = new platillos (8.25, "shrimp roll");
 
- $(".price").text(ap1.precio);
- $(".name").text(ap1.nombre);
- $(".descripcion").text(ap1.descripcion);
+ $(".pck1").text(ck1.precio);
+ $(".nck1").text(ck1.nombre);
+ $(".dck1").text(ck1.descripcion);
 });
