0568a707cbaf7b055ecb7277053a81a8f13b6cfa
omar morales <omarmorales865@gmail.com>
2013-09-09 00:06:36 -0400

    se arreglo el prloblema de js
    ya carga los external js sin tener que hacer refresh

--- js/cofun.js
+++ js/cofun.js
@@ -1,10 +1,14 @@
-$(document).ready(function(){
-	$('#prueba').click(leftp);
-	$('#prueba2').click(rightp);
-	
 
-
-});
+	$('div:jqmData(role="page")').live('pagebeforeshow', listo);
+    // code to execute on each page change
+    function listo () {
+    	alert('si sale este alerta es para probar que los js externos se cargan. si los paneles no se cargans es por same orgin policy')
+    	// body...
+    
+	$('.prueba').ready(leftp);
+	$('.prueba2').ready(rightp);
+	}
+  
 
 function leftp() {
 	$('#pruebapanel').load('panell.html', esto) 
---
+++ js/hola.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	$('#prueba').click(function  () {
+		alert("menuhtml")
+		// body...
+	})
+	// body...
+});
